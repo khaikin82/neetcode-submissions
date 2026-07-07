0772a5c911b9a27b9189dae8ab184b7323a6d836
Hoàng Đăng Khải <124850786+khaikin82@users.noreply.github.com>
2026-07-07 10:33:27 +0700

    Add: products-of-array-discluding-self - submission-1

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-1.go
@@ -0,0 +1,22 @@
+func productExceptSelf(nums []int) []int {
+	n := len(nums)
+	res := make([]int, n)
+
+	for i := 0; i < n; i++ {
+		res[i] = 1
+	}
+
+	prefix := 1
+	for i := 0; i < n; i++ {
+		res[i] = res[i] * prefix
+		prefix *= nums[i]
+	}
+	
+	postfix := 1
+	for i := n-1; i >= 0; i-- {
+		res[i] = res[i] * postfix
+		postfix *= nums[i]
+	}
+
+	return res
+}
