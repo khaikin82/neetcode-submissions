950a3045f9d0d9317fbf7605582ba5ca24afab7e
Hoàng Đăng Khải <124850786+khaikin82@users.noreply.github.com>
2026-08-18 23:25:03 +0700

    Add: two-integer-sum-ii - submission-3

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-3.go
@@ -0,0 +1,14 @@
+func twoSum(numbers []int, target int) []int {
+	l, r := 0, len(numbers)-1
+	for l < r {
+		sum := numbers[l] + numbers[r]
+		if sum < target {
+			l++
+		} else if sum > target {
+			r--
+		} else {
+			return []int{l+1, r+1}
+		}
+	}
+	return []int{}
+}
