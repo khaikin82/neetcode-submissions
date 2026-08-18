c038377ce724f43e633019ed921bf7ba236102ca
Hoàng Đăng Khải <124850786+khaikin82@users.noreply.github.com>
2026-08-18 17:33:35 +0700

    Add: is-palindrome - submission-3

---
+++ Data Structures & Algorithms/is-palindrome/submission-3.go
@@ -0,0 +1,23 @@
+func isPalindrome(s string) bool {
+	i := 0
+	j := len(s)-1
+	s = strings.ToUpper(s)
+	for i < j {
+		for i < j && !isUpperLetterOrNumber(s[i]) {
+			i++
+		}
+		for i < j && !isUpperLetterOrNumber(s[j]) {
+			j--
+		}
+		if s[i] != s[j] {
+			return false
+		}
+		i++
+		j--
+	}
+	return true
+}
+
+func isUpperLetterOrNumber(c byte) bool {
+	return (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9')
+}
