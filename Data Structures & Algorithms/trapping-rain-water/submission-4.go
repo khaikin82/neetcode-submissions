7db5d8404a1d819f9654a9c158bcc7cd7a39474d
Hoàng Đăng Khải <124850786+khaikin82@users.noreply.github.com>
2026-08-20 21:04:01 +0700

    Add: trapping-rain-water - submission-4

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-4.go
@@ -0,0 +1,52 @@
+func trap(height []int) int {
+	cur := 0
+	res := 0
+	for {
+		next, sub := findClosestGreater(height, cur)
+		if next == -1 {
+			break
+		}
+		res += max(min(height[next], height[cur]) * (next-cur-1) - sub, 0)
+		cur = next
+	}
+	for {
+		next, sub := findGreatestSmaller(height, cur)
+		if next == -1 {
+			return res
+		}
+		res += max(min(height[next], height[cur]) * (next-cur-1) - sub, 0)
+		cur = next
+	}
+	return res
+}
+
+func findClosestGreater(height []int, cur int) (int, int) {
+	next := -1
+	sub := 0
+
+	for i := cur + 1; i < len(height); i++ {
+		if height[i] >= height[cur] {
+			return i, sub
+		}
+		sub += height[i]
+	}
+	return next, sub
+}
+
+func findGreatestSmaller(height []int, cur int) (int, int) {
+	next := -1
+	sub, tmp := 0, 0
+	minDiff := math.MaxInt
+	for i := cur + 1; i < len(height); i++ {
+		if height[i] < height[cur] {
+			diff := height[cur] - height[i]
+			if diff <= minDiff {
+				minDiff = diff
+				next = i
+				sub = tmp
+			}
+		}
+		tmp += height[i]
+	}
+	return next, sub
+}
